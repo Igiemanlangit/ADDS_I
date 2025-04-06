3b9be56a2ba930bfdc9692abf60626e00771e78f
igiemanlangit <igie.manlangit@student.adelaide.edu.au>
2025-04-06 20:26:35 +0930

    lectures

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -30,3 +30,45 @@ int main ()
     std::cout << "number: " << fib(n);
     return 0;
 }
+
+program 1
+#include <iostream>
+using namespace std;
+
+void foo(int n)
+{
+    if (n == 0)
+    {
+        return;
+    } else {
+    cout << n;
+    return foo(n-1);
+    }
+
+}
+int main() {
+    foo(3);
+    return 0;
+}
+
+
+program 2
+#include <iostream>
+using namespace std;
+
+void foo(int n)
+{
+    if (n == 0)
+    {
+        return;
+    } else
+    {
+    foo(n-1);
+    cout << n;
+    }
+   
+}
+int main() {
+    foo(3);
+    return 0;
+}
